65d19d2b9cfef19a8c0622d54496b10b17a20235
zhengmin <murphyzh06@gmail.com>
2020-01-13 14:21:37 +0800

    Update files

--- himalayafm/Main/MZCustomTabBarController.swift
+++ himalayafm/Main/MZCustomTabBarController.swift
@@ -24,7 +24,7 @@ class MZCustomTabBarController: ESTabBarController {
         self.title = "Irregularity"
         self.tabBar.shadowImage = UIImage(named: "transparent")
         self.didHijackHandler = {
-            [weak tabBarController] tabbarController, viewController, index in
+            [weak self] tabbarController, viewController, index in
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
                 let warning = MessageView.viewFromNib(layout: .cardView)
@@ -47,13 +47,25 @@ class MZCustomTabBarController: ESTabBarController {
         let discover = MZDiscoverViewController()
         let mine = MZMineViewController()
         
-        home.tabBarItem = ESTabBarItem.init(MZIrregularityContentView(), title: "首页", image: UIImage.init(named: "tab_home_normal"), selectedImage: UIImage.init(named: "tab_home_highlighted"), tag: 0)
-        listen.tabBarItem = ESTabBarItem.init(MZIrregularityContentView(), title: "我听", image: UIImage.init(named: "tab_listen_normal"), selectedImage: UIImage.init(named: "tab_listen_highlighted"), tag: 1)
+        let titles = ["首页", "我听", "播放", "发现", "我的"]
+        
+        home.tabBarItem = ESTabBarItem.init(MZCommonContentView(), title: titles[0], image: UIImage.init(named: "tab_home_normal"), selectedImage: UIImage.init(named: "tab_home_highlighted"), tag: 0)
+        listen.tabBarItem = ESTabBarItem.init(MZCommonContentView(), title: titles[1], image: UIImage.init(named: "tab_listen_normal"), selectedImage: UIImage.init(named: "tab_listen_highlighted"), tag: 1)
         play.tabBarItem = ESTabBarItem.init(MZIrregularityContentView(), title: nil, image: UIImage.init(named: "tab_play_normal"), selectedImage: UIImage.init(named: "tab_play_highlighted"), tag: 2)
-        discover.tabBarItem = ESTabBarItem.init(MZIrregularityContentView(), title: "发现", image: UIImage.init(named: "tab_discover_normal"), selectedImage: UIImage.init(named: "tab_discover_highlighted"), tag: 3)
-        mine.tabBarItem = ESTabBarItem.init(MZIrregularityContentView(), title: "我的", image: UIImage.init(named: "tab_mine_normal"), selectedImage: UIImage.init(named: "tab_mine_highlighted"), tag: 4)
+        discover.tabBarItem = ESTabBarItem.init(MZCommonContentView(), title: titles[3], image: UIImage.init(named: "tab_discover_normal"), selectedImage: UIImage.init(named: "tab_discover_highlighted"), tag: 3)
+        mine.tabBarItem = ESTabBarItem.init(MZCommonContentView(), title: titles[4], image: UIImage.init(named: "tab_mine_normal"), selectedImage: UIImage.init(named: "tab_mine_highlighted"), tag: 4)
         
-        
-        
+        let homeNav = MZNavigationController.init(rootViewController: home)
+        let listenNav = MZNavigationController.init(rootViewController: listen)
+        let playNav = MZNavigationController.init(rootViewController: play)
+        let discoverNav = MZNavigationController.init(rootViewController: discover)
+        let mineNav = MZNavigationController.init(rootViewController: mine)
+        homeNav.title = titles[0]
+        listenNav.title = titles[1]
+        playNav.title = titles[2]
+        discoverNav.title = titles[3]
+        mineNav.title = titles[4]
+    
+        self.viewControllers = [homeNav, listenNav, playNav, discoverNav, mineNav]
     }
 }
--- himalayafm/Main/MZNavigationController.swift
+++ himalayafm/Main/MZNavigationController.swift
@@ -12,11 +12,14 @@ class MZNavigationController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        WRNavigationBar.defaultNavBarBarTintColor = UIColor.init(red: 245.0/255.0, green: 245.0/255.0, blue: 245.0/255.0, alpha: 1.0)
         WRNavigationBar.defaultNavBarTintColor = ButtonColor
         WRNavigationBar.defaultNavBarTitleColor = UIColor.darkText
         WRNavigationBar.defaultShadowImageHidden = true
     }
+    
+    
 }
 
 extension MZNavigationController {
