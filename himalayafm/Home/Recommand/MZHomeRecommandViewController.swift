314add3718bc0f8713cfd8dc2492023f97be5052
zhengmin <murphyzh06@gmail.com>
2020-01-14 17:44:58 +0800

    Update views

--- himalayafm/Home/Recommand/MZHomeRecommandViewController.swift
+++ himalayafm/Home/Recommand/MZHomeRecommandViewController.swift
@@ -11,23 +11,24 @@ import UIKit
 class MZHomeRecommandViewController: UIViewController {
     
     private let kRecommandHeaderViewID = "RecommandHeaderView"
-        private let kRecommandFooterViewID = "RecommandFooterView"
+    private let kRecommandFooterViewID = "RecommandFooterView"
         
-        private let kRecommandHeaderCellID = "RecommandHeaderCell"
-        private let kGuessULikeCellID      = "GuessULikeCell"
-        private let kHotVoiceBookCellID    = "VoiceBookCell"
-        private let kAdvertizingCellID     = "AdvertizingCell"
-        private let kOneTouchListenCellID  = "OneTouchListenCell"
-        private let kRecommandForUCellID   = "RecommandForUCell"
-        private let kHotLiveCellID         = "HotLiveCell"
-    
+    private let kRecommandHeaderCellID = "RecommandHeaderCell"
+    private let kGuessULikeCellID      = "GuessULikeCell"
+    private let kHotVoiceBookCellID    = "VoiceBookCell"
+    private let kAdvertizingCellID     = "AdvertizingCell"
+    private let kOneTouchListenCellID  = "OneTouchListenCell"
+    private let kRecommandForUCellID   = "RecommandForUCell"
+    private let kHotLiveCellID         = "HotLiveCell"
+     
     lazy var conllectionView : UICollectionView = {
         let customCollectionView = UICollectionView(frame:.zero, collectionViewLayout: UICollectionViewLayout())
         customCollectionView.delegate = self as? UICollectionViewDelegate
         customCollectionView.dataSource = self as? UICollectionViewDataSource
-            
-    //    customCollectionView.register(, forCellWithReuseIdentifier: )
-            
+        
+        customCollectionView.register(MZRecommandHeaderView.self, forCellWithReuseIdentifier:kRecommandHeaderViewID)
+        customCollectionView.register(MZRecommandFooterView.self, forCellWithReuseIdentifier: kRecommandFooterViewID)
+        
         return customCollectionView
     }()
     
