c2bdda9275af191ad6458ed6d1f17fcb123f2db3
zhengmin <murphyzh06@gmail.com>
2020-01-14 14:18:00 +0800

    Update files

---
+++ himalayafm/Home/Recommand/Views/MZRecommandFooterView.swift
@@ -0,0 +1,13 @@
+//
+//  MZRecommandFooterView.swift
+//  himalayafm
+//
+//  Created by zm on 2020/1/13.
+//  Copyright © 2020 fusu. All rights reserved.
+//
+
+import UIKit
+
+class MZRecommandFooterView: UICollectionReusableView {
+        
+}
